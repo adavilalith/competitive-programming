e95cafa7c2122ffbdd29b6da2b18d9219b4fcfde
Lalith Adavi <adavilalith11@gmail.com>
2026-06-13 18:50:58 +0530

    creating my standard template for codeforces contest problems

---
+++ template/codeforces_template.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+############ ---- Input Functions ---- ############
+def inp():
+    return(int(input()))
+def inlt():
+    return(list(map(int,input().split())))
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+def invr():
+    return(map(int,input().split()))
+
+t=inp()
+for _ in range(t):
+    pass
+    # Write your code here
